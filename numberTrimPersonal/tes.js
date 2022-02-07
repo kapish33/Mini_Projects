6cd06f8b5c2872a939344990f31fba06d2edd0ec
Kapish Singh <kapishsingh2209@gmail.com>
2022-02-07 14:53:06 +0530

    S

---
+++ numberTrimPersonal/tes.js
@@ -0,0 +1,22 @@
+/*
+[
+    { body: "No" },
+    { body: "Yes" },
+]
+*/
+let text = `!setOptions {key1: value1},{key2: value2}`;
+// !setOptions to "";
+let options = text
+  .replace(/^!setOptions\s*(.*)/, "$1")
+  .replaceAll("{", "")
+  .replaceAll("}", "");
+let optionsArray = options.split(",");
+let optionsObject = [];
+for (let i = 0; i < optionsArray.length; i++) {
+  // push key value pair to optionsObject
+  let keyValue = optionsArray[i].split(":");
+  optionsObject.push({
+    [keyValue[0].trim()]: keyValue[1].trim(),
+  });
+}
+console.log(optionsObject);
