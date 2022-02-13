298a5e67f6d4696b28a35725a57a6af067181777
Kapish Singh <kapishsingh2209@gmail.com>
2022-02-13 16:23:54 +0530

    asas

---
+++ numberTrimPersonal/Js/submitForm.js
@@ -0,0 +1,42 @@
+const sendData = () => {
+  console.log("sendData");
+  const name = document.getElementById("name").value;
+  const number = document.getElementById("number").value;
+  const city = document.getElementById("city").value;
+  if (name === "" || number === "" || city === "") {
+    alert("Please fill all the fields");
+  } else if (number.length < 10) {
+    alert("Please enter a valid number");
+  } else {
+    var myHeaders = new Headers();
+    myHeaders.append("Content-Type", "application/json");
+
+    var raw = JSON.stringify({
+      name: name,
+      phoneNumber: number,
+      city: city,
+    });
+
+    var requestOptions = {
+      method: "POST",
+      headers: myHeaders,
+      body: raw,
+      redirect: "follow",
+    };
+
+    fetch("https://whatsappmeserver.herokuapp.com/users", requestOptions)
+      .then((response) => response.text())
+      .then((result) => console.log(result))
+      .catch((error) => console.log("error", error));
+  }
+};
+const number = document.getElementById("number");
+number.addEventListener("keyup", () => {
+  if (number.value.length > 10) {
+    number.value = number.value.slice(0, 10);
+    alert("Enter Your 10 digit number");
+  }
+  if (isNaN(number.value)) {
+    number.value = number.value.slice(0, number.value.length - 1);
+  }
+});
